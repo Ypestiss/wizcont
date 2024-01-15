b17167cc25766049d6e239049d96436b705495f8
ANuvemKumo <anuvemkumo@gmail.com>
2024-01-15 15:48:12 -0300

    Atualização de UI e UX design
    A interface do main foi completamente remodelada para melhor experiência de usuário (UX) e houve alteração no nome do arquivo "style.css" para "mainStyles.css" e do arquivo "tools.js" para "mainTools.js" por conta de possível incompatibilidade com o servidor.

---
+++ target/wizcount/styles/mainTools.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Seleciona a imagem
+  const userImage = document.getElementById('user');
+
+  // Armazena as fontes das imagens
+  const imageSources = [
+    './icons/icon.svg',
+    './icons/alticon.svg'
+  ];
+
+  // Adiciona um evento de mouseover para trocar a fonte da imagem
+  userImage.addEventListener('mouseover', function() {
+    userImage.src = imageSources[1]; // Troca para a imagem colorida
+  });
+
+  // Adiciona um evento de mouseout para retornar à fonte original da imagem
+  userImage.addEventListener('mouseout', function() {
+    userImage.src = imageSources[0]; // Troca para a imagem branca
+  });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+});
+
